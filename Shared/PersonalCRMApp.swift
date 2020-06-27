705e74763290659b6cd8c46fd5ce24652d5b5d1a
Garry Eves <garry@eves.id.au>
2020-06-27 08:53:13 +0800

    Initial Commit

---
+++ Shared/PersonalCRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCRMApp.swift
+//  Shared
+//
+//  Created by Garry Eves on 27/6/20.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
